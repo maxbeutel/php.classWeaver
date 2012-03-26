275969cb680a8c2b9d031c039489067f626dad41
max <max@ubuntu.localdomain>
2012-03-25 22:58:41 -0700

    just use composer autoload

--- autoload.php
+++ autoload.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once __DIR__ . '/vendor/.composer/autoload.php';
-require_once __DIR__ . '/vendor/nikic/php-parser/lib/bootstrap.php';
-require_once __DIR__ . '/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
